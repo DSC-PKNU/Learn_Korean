b854be0fa7c88e3bf41d07d2ab98dce844789e36
차지원 <chajiwon100785@gmail.com>
2021-01-10 19:48:01 +0900

    clear git cache

--- lib/page_class/StudyStagePage.dart
+++ lib/page_class/StudyStagePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learn_korean_for_children/page_class/StudyDictation.dart';
 
+String img_path = 'images/StudyStagePage';
 // 단어 공부 페이지
 class StudyStagePage extends StatefulWidget {
   @override
@@ -11,54 +12,44 @@ class _StudyStagePageState extends State<StudyStagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //TODO: 뒤로가기 버튼 디자인 수정하기
-      //뒤로가기 버튼
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.keyboard_backspace
-        ),
-        onPressed: (){
-          Navigator.pop(context);
-        },
-        ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      
       //스테이지 버튼
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //뒤로가기 버튼
           Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //위에 세줄
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  stage_button[0],
-                  stage_button[1],
-                  stage_button[2]
-                  
-                ],),
-
-              //아래 세줄
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  stage_button[3],
-                  stage_button[4],
-                  stage_button[5]
-                ],),
+              BackPage(context),
+              SizedBox(height: 200,)
             ],
           ),
-          IconButton(
-            icon: Icon(
-              Icons.arrow_forward_ios,
-              size: 50.0,
-              color: Colors.yellow,
-              ), 
-              onPressed: (){
-                //TODO: 다음 페이지로 넘기기
-              })
+          //스테이지 버튼
+          Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //위에 세줄
+                  Row(
+                    children: <Widget>[
+                      stage_button[0],
+                      stage_button[1],
+                      stage_button[2]
+                    ],),
+
+                  //아래 세줄
+                  Row(
+                    children: <Widget>[
+                      stage_button[3],
+                      stage_button[4],
+                      stage_button[5]
+                    ],),
+                ],
+              ),
+
+              // 다음 페이지로 넘기기 버튼
+              PassStep[0]
+            ],
+          ),
         ],
       ),
      
@@ -76,6 +67,20 @@ List<Widget> stage_button = [
 
 ];
 
+Widget BackPage(BuildContext context){
+  return InkWell(
+  child: Image.asset('$img_path/back_page_blue.png',width: 120, height: 120),
+  onTap: () => Navigator.pop(context)
+  );
+}
+
+List<Widget> PassStep = [
+  InkWell(
+  child: Image.asset('$img_path/step_next.png',width: 120, height: 120),
+  onTap: () {}//TODO: 다음 페이지로 넘기기
+  )
+
+];
 
 class ReusableCard extends StatelessWidget {
 
@@ -104,7 +109,9 @@ class ReusableCard extends StatelessWidget {
   Widget addButton(BuildContext context){
     return InkWell(
             //TODO: 버튼 이미지 할당
-            child: Image.asset('images/emoticon.png', width: 200, height: 400),
+            child: 
+              Image.asset('$img_path/stage_background.png', width: 100, height: 20),
+
             onTap: () => Navigator.push(context, 
                 MaterialPageRoute<void>(
                   builder: (BuildContext context) {
@@ -124,31 +131,15 @@ class ReusableCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //TODO: 버튼 디자인
-            new Row(
-              children: star
+            Row(
+              children: star,
             ),
-
-          FloatingActionButton.extended(    
-            heroTag: round,
-            backgroundColor: Colors.purple,
-            onPressed: (){
-              //화면 전환
-              Navigator.push(context, 
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return cardChild;
-                  }
-                )
-              );
-            },
-            
-            label: Text(
-              '$round단계',
-              style: TextStyle(
-                fontSize: 20.0
-              ),
+            Stack(
+              children: <Widget>[
+                addButton(context),
+                Text('$round단계')
+              ]
             ),
-          )
         ],
         
       ),
