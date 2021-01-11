6a306def162ce2214542735ed2ef4588d28d10fa
milkness <dfhilder@gmail.com>
2021-01-11 15:32:49 +0900

    문제 등록 다이얼로그

--- lib/page_class/RegistProblem.dart
+++ lib/page_class/RegistProblem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class RegistProblem extends StatefulWidget {
   @override
@@ -30,7 +31,10 @@ class _RegistProblemState extends State<RegistProblem> {
                 child: FlatButton(
                   color: Colors.blue[200],
                   onPressed: () {
-                    //문제 등록하는 함수
+                    showDialog(
+                      context: context,
+                      builder: (context) => _dialog(),
+                    );
                   },
                   child: Text(
                     '등록',
@@ -46,6 +50,70 @@ class _RegistProblemState extends State<RegistProblem> {
                   },
                   child: Text(
                     '삭제',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Dialog _dialog() {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Dialog(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '문제 등록',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: screenWidth * 0.02, bottom: 10),
+            child: Text('문제를 입력하세요'),
+          ),
+          Container(
+              padding: EdgeInsets.only(
+                  left: screenWidth * 0.02,
+                  right: screenWidth * 0.02,
+                  bottom: 20),
+              child: TextFormField(
+                expands: false,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+              )),
+              
+          Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue[200],
+                  onPressed: () {
+                    //문제 등록하는 함수
+                  },
+                  child: Text(
+                    '확인',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red[300],
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    '취소',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
